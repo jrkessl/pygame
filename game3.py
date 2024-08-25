71f890e0dcfbca75ea28ae925fcaf5ee405a65bc
Juliano Kessler <juliano.kessler@ticketmaster.com>
2024-08-25 16:44:39 -0300

    comentei o enemy

--- game3.py
+++ game3.py
@@ -22,49 +22,51 @@ WHITE = (255, 255, 255)
 # SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1700
 SCREEN_HEIGHT = 800
-altura_jogador = 64 
-largura_jogador = 64
+# altura_jogador = 64 
+# largura_jogador = 64
+altura_jogador = 128
+largura_jogador = 128
  
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Joguinho nervoso")
  
 
-class Enemy(pygame.sprite.Sprite):
-    left = False 
-    contadorVirada = 0
+# class Enemy(pygame.sprite.Sprite):
+#     left = False 
+#     contadorVirada = 0
 
-    def __init__(self):
-        super().__init__() 
-        self.image = pygame.image.load("Enemy1.png") # carrega a sprite do inimigo
-        self.rect = self.image.get_rect() # cria a caixa de colisão 
-        self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0) 
+#     def __init__(self):
+#         super().__init__() 
+#         self.image = pygame.image.load("Enemy1.png") # carrega a sprite do inimigo
+#         self.rect = self.image.get_rect() # cria a caixa de colisão 
+#         self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0) 
  
-    def move(self):
-        Enemy.contadorVirada = Enemy.contadorVirada + 1 
-        random_number = random.randint(0, 1) 
-        # print(f"random_number={random_number}")
+#     def move(self):
+#         Enemy.contadorVirada = Enemy.contadorVirada + 1 
+#         random_number = random.randint(0, 1) 
+#         # print(f"random_number={random_number}")
 
-        if(Enemy.contadorVirada > 40):
-            if (random_number == 0):
-                Enemy.left = True
-            else:
-                Enemy.left = False
-            Enemy.contadorVirada = 0
+#         if(Enemy.contadorVirada > 40):
+#             if (random_number == 0):
+#                 Enemy.left = True
+#             else:
+#                 Enemy.left = False
+#             Enemy.contadorVirada = 0
             
 
-        # print(f"Enemy.left={Enemy.left}")
-        if (Enemy.left):
-            self.rect.move_ip(-1,5)
-        else:
-            self.rect.move_ip(1,5)
+#         # print(f"Enemy.left={Enemy.left}")
+#         if (Enemy.left):
+#             self.rect.move_ip(-1,5)
+#         else:
+#             self.rect.move_ip(1,5)
 
-        if (self.rect.bottom > SCREEN_HEIGHT):
-            self.rect.top = 0
-            self.rect.center = (random.randint(30, 370), 0)
+#         if (self.rect.bottom > SCREEN_HEIGHT):
+#             self.rect.top = 0
+#             self.rect.center = (random.randint(30, 370), 0)
  
-    def draw(self, surface):
-        surface.blit(self.image, self.rect) 
+#     def draw(self, surface):
+#         surface.blit(self.image, self.rect) 
  
 class Player(pygame.sprite.Sprite):
     animationCounter=0
@@ -272,14 +274,14 @@ class Player(pygame.sprite.Sprite):
  
          
 P1 = Player()
-E1 = Enemy()
+# E1 = Enemy()
 
 #Creating Sprites Groups
-enemies = pygame.sprite.Group()
-enemies.add(E1)
-all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
-all_sprites.add(E1)
+# enemies = pygame.sprite.Group()
+# enemies.add(E1)
+# all_sprites = pygame.sprite.Group()
+# all_sprites.add(P1)
+# all_sprites.add(E1)
 
 while True:     
     for event in pygame.event.get():              
@@ -287,25 +289,25 @@ while True:
             pygame.quit()
             sys.exit()
     P1.update()
-    E1.move()
+    # E1.move()
 
     # detecção de colisão
-    if pygame.sprite.spritecollideany(P1, enemies): # detecta colisão 
-        new_image = pygame.image.load("explodido-generico.png") # carrega imagem 
-        P1.image = new_image # seta nova imagem no player e enemy 
-        E1.image = new_image
-        DISPLAYSURF.blit(E1.image, E1.rect) # "renderiza" a nova imagem no player e enemy 
-        DISPLAYSURF.blit(P1.image, P1.rect)
-        pygame.display.update() # atualiza a tela 
-        # for entity in all_sprites: # remove todas as sprites da tela 
-        #     entity.kill() 
-        time.sleep(2)
-        pygame.quit()
-        sys.exit()     
+    # if pygame.sprite.spritecollideany(P1, enemies): # detecta colisão 
+    #     new_image = pygame.image.load("explodido-generico.png") # carrega imagem 
+    #     P1.image = new_image # seta nova imagem no player e enemy 
+    #     E1.image = new_image
+    #     DISPLAYSURF.blit(E1.image, E1.rect) # "renderiza" a nova imagem no player e enemy 
+    #     DISPLAYSURF.blit(P1.image, P1.rect)
+    #     pygame.display.update() # atualiza a tela 
+    #     # for entity in all_sprites: # remove todas as sprites da tela 
+    #     #     entity.kill() 
+    #     time.sleep(2)
+    #     pygame.quit()
+    #     sys.exit()     
      
     DISPLAYSURF.fill(WHITE)
     P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+    # E1.draw(DISPLAYSURF)
          
     pygame.display.update()
     FramePerSec.tick(FPS)
